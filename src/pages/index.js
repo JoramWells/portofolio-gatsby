caf2d5b8798deef76bad4f2a0384fdca9d2c13b5
unknown <jorammanoah1@gmail.com>
2021-05-05 12:36:54 -0700

    Added images

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,40 +4,63 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import {Card,Row,Col} from 'antd'
+import { Card, Row, Col } from "antd"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h2 style={{textAlign:"center"}}>Fast, Reliable, Secure</h2>
+    <h2 style={{ textAlign: "center" }}>Fast, Reliable, Secure</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Row justify="space-around">
       <Col>
-      <Card>
-        <h3>Fast</h3>
-      </Card>
+        <Card
+          cover={
+            <StaticImage
+              src="../images/fast.svg"
+              width={200}
+              quality={95}
+              formats={["AUTO", "WEBP", "AVIF"]}
+              alt="A fast"
+            />
+          }
+        >
+          <h3>Fast</h3>
+        </Card>
       </Col>
       <Col>
-      <Card>
-        <h3>Reliable</h3>
-      </Card>
+        <Card
+          cover={
+            <StaticImage
+              src="../images/Success.svg"
+              alt="success"
+              width={200}
+              quality={95}
+              formats={["AUTO", "WEBP", "AVIF"]}
+              style={{marginBottom:"2rem"}}
+            />
+          }
+        >
+          <h3>Reliable</h3>
+        </Card>
       </Col>
       <Col>
-      <Card>
-        <h3>Secure</h3>
-      </Card>
+        <Card
+          cover={
+            <StaticImage
+              src="../images/Server.svg"
+              alt="server"
+              width={200}
+              quality={95}
+              formats={["AUTO", "WEBP", "AVIF"]}
+            />
+          }
+        >
+          <h3>Secure</h3>
+        </Card>
       </Col>
     </Row>
 
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
