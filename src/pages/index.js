6ecd96331f1d144201936ca12bdc1ac54db8e422
unknown <jorammanoah1@gmail.com>
2021-05-05 12:06:25 -0700

    Another commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import {Card,Row,Col} from 'antd'
 
 const IndexPage = () => (
   <Layout>
@@ -11,6 +12,24 @@ const IndexPage = () => (
     <h2 style={{textAlign:"center"}}>Fast, Reliable, Secure</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Row justify="space-around">
+      <Col>
+      <Card>
+        <h3>Fast</h3>
+      </Card>
+      </Col>
+      <Col>
+      <Card>
+        <h3>Reliable</h3>
+      </Card>
+      </Col>
+      <Col>
+      <Card>
+        <h3>Secure</h3>
+      </Card>
+      </Col>
+    </Row>
+
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
