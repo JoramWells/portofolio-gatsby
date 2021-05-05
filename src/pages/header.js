df2b6a00c64f7131eb9d2e9a0f85dc66a968455c
unknown <jorammanoah1@gmail.com>
2021-05-05 13:23:12 -0700

    Removed other dependancies

---
+++ src/pages/header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const HeaderPage = () =>(
+        <div>
+            <h1> header</h1>
+        </div>
+    )
+export default HeaderPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,81 +1,80 @@
 import * as React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
 import { Card, Row, Col } from "antd"
+// import Layout from "../components/layout"
+// import Seo from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h2 style={{ textAlign: "center" }}>Fast, Reliable, Secure</h2>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Row justify="space-around" style={{ fontFamily: "Arial" }}>
-      <Col>
-        <Card
-          cover={
-            <StaticImage
-              src="../images/secure.svg"
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-              alt="A fast"
-            />
-          }
-          style={{ width: "15rem", border:"0" }}
-        >
-          <h3>Secure</h3>
-          <p style={{ color: "grey" }}>
-            Google authentication for fast user signin, encrypted data,
-            increased cyber crimes, protect user data
-          </p>
-        </Card>
-      </Col>
-      <Col>
-        <Card
-          cover={
-            <StaticImage
-              src="../images/stars.svg"
-              alt="success"
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-            />
-          }
-          style={{ width: "15rem", border:"0" }}
-        >
-          <h3>Fast</h3>
-          <p style={{color:"grey", fontSize:"14px"}}>
-            Applications that run fast and smoothless, ensure amazing client
-            experience
-          </p>
-        </Card>
-      </Col>
-      <Col>
-        <Card
-          cover={
-            <StaticImage
-              src="../images/success.svg"
-              alt="server"
-              quality={95}
-              formats={["AUTO", "WEBP", "AVIF"]}
-            />
-          }
-          style={{ width: "15rem", border:"0" }}
-        >
-          <h3>Reliable</h3>
-          <p style={{color:"grey", fontSize:"14px"}}>
-            Applications that meet user expectations and your companies demands.
-          </p>
-        </Card>
-      </Col>
+  <>
+    {/* <Seo title="Unable" /> */}
+    <Row
+      justify="space-around"
+      align="middle"
+      style={{ fontFamily: "Arial", backgroundColor: "whitesmoke", paddingTop:".5rem",marginBottom:".5rem" }}
+    >
+      <h1>Fast,Reliable and secure</h1>
     </Row>
-
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+      <Row justify="space-around" style={{ fontFamily: "Arial" }}>
+        <Col>
+          <Card
+            cover={
+              <StaticImage
+                src="../images/secure.svg"
+                quality={95}
+                formats={["AUTO", "WEBP", "AVIF"]}
+                alt="A fast"
+              />
+            }
+            style={{ width: "16rem", border: "0" }}
+          >
+            <h3>Secure</h3>
+            <p style={{ color: "grey" }}>
+              Google authentication for fast user signin, encrypted data,
+              increased cyber crimes, protect user data
+            </p>
+          </Card>
+        </Col>
+        <Col>
+          <Card
+            cover={
+              <StaticImage
+                src="../images/stars.svg"
+                alt="success"
+                quality={95}
+                formats={["AUTO", "WEBP", "AVIF"]}
+              />
+            }
+            style={{ width: "15rem", border: "0" }}
+          >
+            <h3>Fast</h3>
+            <p style={{ color: "grey", fontSize: "14px" }}>
+              Applications that run fast and smoothless, ensure amazing client
+              experience
+            </p>
+          </Card>
+        </Col>
+        <Col>
+          <Card
+            cover={
+              <StaticImage
+                src="../images/success.svg"
+                alt="server"
+                quality={95}
+                formats={["AUTO", "WEBP", "AVIF"]}
+              />
+            }
+            style={{ width: "16rem", border: "0" }}
+          >
+            <h3>Reliable</h3>
+            <p style={{ color: "grey", fontSize: "14px" }}>
+              Applications that meet user expectations and your companies
+              demands.
+            </p>
+          </Card>
+        </Col>
+      </Row>
+  </>
 )
 
 export default IndexPage
